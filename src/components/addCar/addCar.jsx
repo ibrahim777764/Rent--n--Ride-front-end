c693c32e6ba70fa7ae65645e09454a21629d7a76
brainconnect93 <brainconnect0@gmail.com>
2022-11-17 23:58:47 +0100

    Add the necessary form type to the addcar

--- src/components/addCar/addCar.jsx
+++ src/components/addCar/addCar.jsx
@@ -1,27 +1,164 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
 import './addCar.scss';
 
-class AddCar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      visible: false,
-    };
-  }
+const AddCar = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
 
-  render () {
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div className='car-head'>
+    <div className="car-head">
       <h1>I Love Lamborghini</h1>
 
-      <form>
-        <label>Name: </label> <br />
-        <input type='text' name='name' /> <br />
+      <form onSubmit={handleSubmit(onSubmit)}>
+
+      <div className="grid-display grid-simple">
+        <div className="add-margin-below">
+          <label htmlFor="brand">Brand</label>
+          <input
+            type="text"
+            id="brand"
+            name="brand"
+            placeholder="Ferrari"
+            className="form-field"
+            required
+          />
+        </div>
+      </div>
+
+      <div className="grid-display grid-double">
+        <div className="add-margin-below">
+          <label htmlFor="brand">Model</label>
+          <input
+            type="text"
+            id="model"
+            name="model"
+            placeholder="Testarossa"
+            className="form-field"
+            required
+          />
+        </div>
+
+        <div className="add-margin-below grid-double">
+          <label htmlFor="year">Year</label>
+          <input
+            type="text"
+            id="year"
+            name="year"
+            placeholder="2018"
+            className="form-field"
+            required
+          />
+        </div>
+      </div>
+
+      <div className="grid-display grid-double">
+        <div className="add-margin-below grid-double">
+          <label htmlFor="country">Country</label>
+          <input
+            type="text"
+            id="country"
+            name="country"
+            placeholder="Italia"
+            className="form-field"
+            required
+          />
+        </div>
+
+        <div className="add-margin-below">
+          <label htmlFor="vehicle">Color</label>
+          <select
+            name="vehicle"
+            className="form-field"
+            // defaultValue={vehicle || ''}
+            required
+          >
+            <option value="">Select one</option>
+            <option value="Black">Black</option>
+            <option value="Blue">Blue</option>
+            <option value="Brown">Brown</option>
+            <option value="Carbon">Carbon</option>
+            <option value="Green">Green</option>
+            <option value="Orange">Orange</option>
+            <option value="Red">Red</option>
+            <option value="Silver">Silver</option>
+            <option value="Yellow">Yellow</option>
+            <option value="White">White</option>
+          </select>
+        </div>
+      </div>
+
+      <h3>Technical Specs</h3>
+
+      <div className="grid-display grid-triple">
+        <div className="add-margin-below">
+          <label htmlFor="power">Power</label>
+          <input
+            type="text"
+            id="power"
+            name="power"
+            placeholder="780 CV (574 kW)"
+            className="form-field"
+            required
+          />
+        </div>
+
+        <div className="add-margin-below">
+          <label htmlFor="max_speed">Max Speed</label>
+          <input
+            type="text"
+            id="max_speed"
+            name="max_speed"
+            placeholder="355 km/h"
+            className="form-field"
+            required
+          />
+        </div>
+
+        <div className="add-margin-below">
+          <label htmlFor="acceleration">Acceleration</label>
+          <input
+            type="text"
+            id="acceleration"
+            name="acceleration"
+            placeholder="9.8s"
+            className="form-field"
+            required
+          />
+        </div>
+      </div>
+
+      <h3>Description</h3>
+
+      <div className="add-margin-below">
+        <textarea
+          name="description"
+          className="form-field"
+          required
+        />
+      </div>
+        {/* <div className="form-control">
+          <label>Name</label>
+          <input type="text" name="email" {...register("email")} />
+        </div>
+        <div className="form-control">
+          <label>Password:</label>
+          <input type="password" name="password" {...register("password")} />
+        </div>
+        <div className="form-control">
+          <label></label>
+          <button type="submit">Login</button>
+        </div> */}
       </form>
     </div>
-  )
- }
+  );
 }
 
 export default AddCar
-
