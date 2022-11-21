2dbb2bf69c4d5083cb2658fccbc24d141fbad404
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-11-21 11:22:23 +0200

    Add reducers in the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,19 @@
 /* eslint-disable linebreak-style */
 import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'reduxgit';
+import loginReducer from './auth/login';
+import signUpReducer from './auth/signup';
 import vehiclesReducer from './vehicles/vehicles';
+
+const rootReducer = combineReducers({
+  login: loginReducer,
+  signup: signUpReducer,
+});
 
 const store = configureStore({
   reducer: {
     vehicles: vehiclesReducer,
+    rootReducer,
   },
 });
 
