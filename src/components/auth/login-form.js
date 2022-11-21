a336f43328f5cc5574a892ef221bcc48dc917966
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-11-21 13:00:35 +0200

    Add login in form and sign up form

---
+++ src/components/auth/login-form.js
@@ -0,0 +1,73 @@
+/* eslint-disable linebreak-style */
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, useLocation } from 'react-router-dom';
+import './auth.scss';
+import { login } from '../../redux/auth/login';
+
+function loginForm({ setUserSession, userSession }) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const signupSuccess = useSelector((state) => state.signUp.success);
+  const location = useLocation();
+
+  useEffect(() => {
+    if (userSession && location.pathname === '/login') navigate('/');
+  }, [location]);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const formObject = new FormData(e.target);
+    const data = Object.fromEntries(formObject.entries());
+
+    const response = await dispatch(login(data));
+    if ('token' in response.payload) setUserSession(response.payload);
+    navigate('/');
+  };
+  return (
+    <div className="container page-login">
+
+      <form className="login-form" method="POST" onSubmit={onSubmit}>
+        <p className="success-message">{ signupSuccess }</p>
+
+        <div className="add-padding-below">
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="form-field"
+            placeholder="Email"
+            required
+          />
+        </div>
+
+        <div className="add-padding-below">
+          <input
+            type="text"
+            id="password"
+            name="password"
+            className="form-field"
+            placeholder="Password"
+            required
+          />
+        </div>
+
+        <div className="form-bottom-bar">
+          <button type="submit" className="submit-button" size="main" color="dark">Login</button>
+        </div>
+      </form>
+
+    </div>
+  );
+}
+
+// eslint-disable-next-line react/no-typos
+loginForm.PropTypes = {
+  userSession: PropTypes.oneOfType([
+    PropTypes.objectOf(PropTypes.string),
+    PropTypes.oneOf(['null', 'undefined']),
+  ]).isRequired,
+};
+
+export default loginForm;
---
+++ src/components/auth/signup-form.js
@@ -0,0 +1,93 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable react/no-typos */
+/* eslint-disable react/prop-types */
+/* eslint-disable linebreak-style */
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { signUp } from '../../redux/auth/signup';
+
+function SignupForm({ userSession }) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (userSession && location.pathname === '/signup') navigate('/');
+  }, [location]);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const formObject = new FormData(e.target);
+    const data = Object.formEntries(formObject.entries());
+
+    dispatch(signUp(data));
+    navigate('/login');
+  };
+  return (
+    <div className="container page-login">
+      <form className="login-form" method="POST" onSubmit={onSubmit}>
+        <h2>SIGNUP</h2>
+
+        <div className="add-padding-below">
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="form-field"
+            placeholder="Name"
+            required
+          />
+        </div>
+
+        <div className="add-padding-below">
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="form-field"
+            placeholder="Email"
+            required
+          />
+        </div>
+
+        <div className="add-padding-below">
+          <input
+            type="text"
+            id="password"
+            name="password"
+            className="form-field"
+            placeholder="Password"
+            required
+          />
+        </div>
+
+        <div className="add-padding-below">
+          <input
+            type="text"
+            id="password"
+            name="password_confirmation"
+            className="form-field"
+            placeholder="confirm password"
+            required
+          />
+        </div>
+
+        <div className="form-bottom-bar">
+          <button type="submit" size="main" className="submit-button" color="dark">Sign up</button>
+        </div>
+      </form>
+
+    </div>
+  );
+}
+
+SignupForm.PropTypes = {
+  userSession: PropTypes.oneOfType([
+    PropTypes.objectOf(PropTypes.string),
+    PropTypes.oneOf(['null', 'undefined']),
+  ]).isRequired,
+};
+
+export default SignupForm;
