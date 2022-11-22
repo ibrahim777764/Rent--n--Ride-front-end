f24f5a89296bdd53c68025b0365ed3024c40fcff
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-11-22 20:30:02 +0200

    Add api links

--- src/redux/auth/signUpAction.js
+++ src/redux/auth/signUpAction.js
@@ -1,40 +1,42 @@
-import { LOADING_USER, POST_USER_SUCCESS, POST_USER_ERRORS, POST_USER_FAILURE } from './Action'
-
+/* eslint-disable linebreak-style */
+import {
+  LOADING_USER, POST_USER_SUCCESS, POST_USER_ERRORS, POST_USER_FAILURE,
+} from './Action';
 
 // const url = 'https://protected-sea-38971.herokuapp.com/api/v1/users'
-const API_URL = 'http://127.0.0.1:3001/api/v1/users'
+const API_URL = 'https://protected-sea-38971.herokuapp.com/api/v1/users';
 
-
-export const signupUser = ({ username, email, password, password_confirmation }) => (dispatch) => {
+export const signupUser = ({
+  username, email, password, password_confirmation,
+}) => (dispatch) => {
   // console.log(user_name)
-  dispatch({ type: LOADING_USER })
+  dispatch({ type: LOADING_USER });
 
   fetch(API_URL, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
     },
     body: JSON.stringify({
       username,
       email,
       password,
-      password_confirmation
-    })
+      password_confirmation,
+    }),
   })
-    .then(res => res.json())
-    .then(data => {
+    .then((res) => res.json())
+    .then((data) => {
       if (data.user !== undefined) {
-        localStorage.setItem("token", data.jwt)
-        alert("Successfully Signed Up")
-        return dispatch({ type: POST_USER_SUCCESS, payload: data })
-      } else {
-        alert(data.errors.map(error => error))
-        return dispatch({ type: POST_USER_ERRORS, payload: data })
+        localStorage.setItem('token', data.jwt);
+        alert('Successfully Signed Up');
+        return dispatch({ type: POST_USER_SUCCESS, payload: data });
       }
+      alert(data.errors.map((error) => error));
+      return dispatch({ type: POST_USER_ERRORS, payload: data });
     })
-    .catch(err => {
-      alert("Unable to SignUp At This Time")
-      return dispatch({ type: POST_USER_FAILURE, payload: err })
-    })
-}
+    .catch((err) => {
+      alert('Unable to SignUp At This Time');
+      return dispatch({ type: POST_USER_FAILURE, payload: err });
+    });
+};
