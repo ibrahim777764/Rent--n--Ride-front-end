4d28904622c8ad5d5c9ab5de8d87e8281ff821a0
Favourezeugwa <favourezeugwa@gmail.com>
2022-11-22 07:01:53 +0100

    Add jest test for all components

---
+++ src/_test_/addCar.test.js
@@ -0,0 +1,16 @@
+import renderer from 'react-test-renderer';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AddCar from '../components/addCar/AddCar';
+import Navbar from '../components/navbar/Navbar';
+
+it('Test that AddCar renders correctly', () => {
+  const tree = renderer
+    .create(
+      <Router>
+        <AddCar />
+        <Navbar />
+      </Router>,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
