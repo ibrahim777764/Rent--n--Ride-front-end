13371c3b2a4ff1c51186aa477a7e56f008f47cef
MoRaad97 <89518488+MoRaad97@users.noreply.github.com>
2022-11-23 16:14:48 +0300

    Update the to production url

--- src/redux/auth/loginAction.js
+++ src/redux/auth/loginAction.js
@@ -1,13 +1,13 @@
 
 import { LOADING_USER, SIGN_IN_SUCCESS, SIGN_IN_ERRORS, SIGN_IN_FAILURE } from './Action'
 
-const API_URL = 'http://127.0.0.1:3001/api/v1'
+const API_URL = 'https://protected-sea-38971.herokuapp.com/api/v1/login'
 
 export const signinUser = ({ email, password }) => (dispatch) => {
   // console.log(user_name)
   dispatch({ type: LOADING_USER })
 
-  fetch(API_URL + '/login', {
+  fetch(API_URL, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
