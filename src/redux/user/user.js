3f92a0b7b6aedbb9744de477aab8ff35b4bd5b1d
Favourezeugwa <favourezeugwa@gmail.com>
2022-11-25 05:27:46 +0100

    Implement userSlice and reducers

--- src/redux/user/user.js
+++ src/redux/user/user.js
@@ -29,6 +29,49 @@ export const signup = createAsyncThunk(
     }
 );
 
+export const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        logout: (state) => {
+            state.user = {};
+            state.islogin = false;
+        },
+
+        status: (state,action)=> {
+             state.islogin=action.payload
+             console.log(state.islogin);
+        }
+    },
+    extraReducers: {
+        [login.pending]: (state) => {
+            state.loading = true;
+        },
+        [login.fulfilled]: (state, action) => {
+            state.loading = false;
+            state.user = action.payload;
+            state.islogin = true;
+            console.log(state.islogin);
+            window.location.href = '/';
+        },
+        [login.rejected]: (state, action) => {
+            state.loading = false;
+            state.error = action.error.message;
+        },
+        [signup.pending]: (state) => {
+            state.loading = true;
+        },
+        [signup.fulfilled]: (state, action) => {
+            state.loading = false;
+            state.user = action.payload;
+            state.islogin = true;
+        },
+        [signup.rejected]: (state, action) => {
+            state.loading = false;
+            state.error = action.error.message;
+        }
+    }
+});
 
 export const {logout,status} = userSlice.actions;
 
