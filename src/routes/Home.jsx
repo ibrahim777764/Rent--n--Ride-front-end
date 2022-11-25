d163e9ea157f382af9a955da9b4f342e104550e0
Favourezeugwa <favourezeugwa@gmail.com>
2022-11-25 05:44:29 +0100

    Modify vehicles slider and cars container

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -41,7 +41,33 @@ function Home() {
   console.log(vehicles);
   return (
 
-    
+    <div className="container">
+      <div className="home-container">
+        <header className="cars-header">
+          <h1>LATEST MODELS</h1>
+          <p>Please select a car</p>
+          <hr className="division-span" />
+        </header>
+        <div className="cars-container">
+          <ul className="car-slider">
+            <Slider {...settings}>
+              {
+                vehicles.cars.map((car, index) => (
+                  <CarCard
+                    key={car.id}
+                    index={index}
+                    name={car.name}
+                    description={car.description}
+                    image={car.image}
+                    id={car.id}
+                  />
+                ))
+              }
+            </Slider>
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
 
