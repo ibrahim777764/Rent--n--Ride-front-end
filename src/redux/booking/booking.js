da656a5f3c2230fa9412d52696ab612952dea7c8
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-11-25 06:12:09 +0200

    Create booking file to handle all bookings

---
+++ src/redux/booking/booking.js
@@ -0,0 +1,35 @@
+import {createSlice,createAsyncThunk} from '@reduxjs/toolkit';
+import { bookingService } from '../../services/services';
+
+const initialState = {
+    bookings: [],
+    loading: false,
+    error: null,
+};
+
+export const fetchBookings = createAsyncThunk(
+    'bookings/fetchBookings',
+    async (id) => {
+        const response = await bookingService.getBookings(id);
+        return response.data;
+    }
+);
+
+
+export const createBooking = createAsyncThunk(
+    'bookings/createBooking',
+    async (data) => {
+        const response = await bookingService.createBooking(data);
+        return response.data;
+    }
+);
+
+export const removeBooking = createAsyncThunk(
+    'bookings/removeBooking',
+    async (id) => {
+        const response = await bookingService.removeBooking(id);
+        return response.data;
+    }
+);
+
+export default bookingsSlice.reducer;
