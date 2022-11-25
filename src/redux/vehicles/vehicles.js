4953f577b37c389ee7685d8a010190fab101e0d7
Favourezeugwa <favourezeugwa@gmail.com>
2022-11-25 05:31:14 +0100

    Add vehicleSlice and reducers

--- src/redux/vehicles/vehicles.js
+++ src/redux/vehicles/vehicles.js
@@ -50,7 +50,70 @@ export const removeVehicle = createAsyncThunk(
   }
 );
 
-
+export const vehiclesSlice = createSlice({
+  name: "vehicles",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [fetchVehicles.pending]: (state) => {
+      state.loading = true;
+    },
+    [fetchVehicles.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.cars = action.payload;
+      console.log(state);
+    },
+    [fetchVehicles.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.error.message;
+    },
+    [fetchVehicle.pending]: (state) => {
+      state.loading = true;
+    },
+    [fetchVehicle.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.car = action.payload;
+    },
+    [fetchVehicle.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.error.message;
+    },
+    [createVehicle.pending]: (state) => {
+      state.loading = true;
+    },
+    [createVehicle.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.cars.push(action.payload);
+    },
+    [createVehicle.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.error.message;
+    },
+    [updateVehicle.pending]: (state) => {
+      state.loading = true;
+    },
+    [updateVehicle.fulfilled]: (state, action) => {
+      state.loading = false;
+      const index = state.cars.findIndex((car) => car.id === action.payload.id);
+      state.cars[index] = action.payload;
+    },
+    [updateVehicle.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.error.message;
+    },
+    [removeVehicle.pending]: (state) => {
+      state.loading = true;
+    },
+    [removeVehicle.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.cars = state.cars.filter((car) => car.id !== action.payload.id);
+    },
+    [removeVehicle.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.error.message;
+    },
+  },
+});
 
 
 export default vehiclesSlice.reducer;
